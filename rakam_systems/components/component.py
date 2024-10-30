828ab12428d6e864a0cd529eb0430e22899e81aa
jeandeb <johnydebod@gmail.com>
2024-10-30 12:53:28 +0100

    compoennt connectorn

--- rakam_systems/components/component.py
+++ rakam_systems/components/component.py
@@ -4,7 +4,7 @@ class Component(ABC):
     """
     An abstract base class for components that can be extended for various functionalities.
     """
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, system_config_path: str, **kwargs) -> None:
         pass
     
     @abstractmethod
