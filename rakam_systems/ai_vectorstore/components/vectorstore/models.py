9b61b21d63043bb6acb3b74c5904ad0cc16cff83
Peng Zheng <curtis.zp@outlook.com>
2025-11-12 14:02:15 +0100

    feat: add Django models for Vector Store to enable automatic model discovery

---
+++ rakam_systems/ai_vectorstore/components/vectorstore/models.py
@@ -0,0 +1,11 @@
+"""
+Django models for Vector Store.
+
+This module re-exports models from pg_models to make them discoverable by Django's
+automatic model discovery mechanism, which looks for a models.py file.
+"""
+from ai_vectorstore.components.vectorstore.pg_models import Collection  # noqa: F401
+from ai_vectorstore.components.vectorstore.pg_models import NodeEntry  # noqa: F401
+
+__all__ = ["Collection", "NodeEntry"]
+
