ff99601d7300a00b5ee12013c3b79f9620e07c91
somebodyawesome-dev <luckynoob2011830@gmail.com>
2026-02-10 23:44:41 +0100

    ft: added ai_utils module in core to tools

---
+++ tools/src/rakam_systems_tools/utils/s3/__init__.py
@@ -0,0 +1,16 @@
+"""
+AI utilities for the Rakam System Core.
+"""
+
+from . import s3
+from . import logging
+from . import metrics
+from . import tracing
+
+__all__ = [
+    "s3",
+    "logging",
+    "metrics",
+    "tracing",
+]
+
